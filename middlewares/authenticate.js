0b12af5d24c7e2525b9c5e44c7f125a1ae7ccd3a
jonathan <82114500+Jonathanthedeveloper@users.noreply.github.com>
2023-03-08 09:24:00 +0100

    FEAT: added authentication middleware to user routes

---
+++ middlewares/authenticate.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const AppError = require('../utils/AppErrorUtil');
+/**
+ *
+ * @param {Request} req 
+ * @param {Response} res 
+ * @param {NextFunction} next 
+ */
+function authenticate(req, res, next) {
+    const token = req.cookies.token || req.headers?.authorization?.split(' ')[1];
+
+    if (!token) return next(new AppError('Access denied. No token provided', 401));
+
+    try {
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_TOKEN);
+        req.user = decoded;
+        next();
+
+    } catch (error) {
+        next(new AppError('Invalid token', 400));
+    }
+}
+
+module.exports = authenticate;
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const authRoute = require('./authRoute');
 const userRoute = require('./userRoute');
+const authenticate = require('../middlewares/authenticate');
 
 router.use('/auth', authRoute)
-router.use('/users', userRoute)
+router.use('/users', authenticate, userRoute)
 
 module.exports = router
