199297eb4559f5ccce07d0c353f3ddf743d60d2d
jonathan <82114500+Jonathanthedeveloper@users.noreply.github.com>
2023-03-10 21:15:25 +0100

    FEAT: added validation to all available endpoints

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const authenticate = require('../middlewares/authenticate');
+const { validateParams, schemas: { handle } } = require('../middlewares/validator');
+
+
 const { getAUser, getAllUsers, editAUser, deleteUser } = require('../controllers/userController');
 
 
-router.get('/:handle', getAUser)
-router.put('/:handle', editAUser);
-router.delete('/:handle', deleteUser);
-router.get('/', getAllUsers)
+router.get('/:handle', validateParams(handle), authenticate, getAUser)
+router.put('/:handle', validateParams(handle), authenticate, editAUser);
+router.delete('/:handle', validateParams(handle), authenticate, deleteUser);
+router.get('/', authenticate, getAllUsers)
 
 module.exports = router;
