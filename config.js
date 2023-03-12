2ad2c15fc5b37ccff6ec3d647bb3e0678d448c53
jonathan <82114500+Jonathanthedeveloper@users.noreply.github.com>
2023-03-12 17:07:23 +0100

    FEAT: added a reset password functionality

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 exports.PORT = process.env.PORT || 3000;
-exports.JWT_EXPIRES_IN = 1000 * 60 * 60
+exports.JWT_EXPIRES_IN = 1000 * 60 * 60;
+exports.PASSWORD_RESET_EXPIRES_IN = 600000;
--- utils/APIFeaturesUtil.js
+++ utils/APIFeaturesUtil.js
@@ -22,7 +22,7 @@ class APIFeatures {
             const fields = this.queryString.fields.split(',').join(' ');
             this.query = this.query.select(fields);
         } else {
-            this.query = this.query.select('-__v -password -isDeleted');
+            this.query = this.query.select('-__v -password -isDeleted,, -resetPassword -resetPasswordExpires');
         }
         return this
     }
