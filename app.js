948051f3b5f5f845ec306593ca7bc459b6e9fe5f
jonathan <82114500+Jonathanthedeveloper@users.noreply.github.com>
2023-03-12 17:38:17 +0100

    FEAT: added rate limiting for prevention of DDOS or bruteforce attack

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const ejs = require('ejs');
 const cors = require('cors');
 const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 
 
 const rootRoute = require('./routes/indexRoute')
@@ -13,6 +14,13 @@ const AppError = require('./utils/AppErrorUtil');
 
 
 const app = express();
+
+// configuring rate limiter
+const limiter = rateLimit({
+    max: 3,
+    windowMs: 5000,
+    message: 'Too many requests from this IP, please try again in 5 seconds'
+})
 
 
 // Setting Up server configurations and middlewares
@@ -23,6 +31,7 @@ app.use(express.json()); // tells express to use json
 app.use(morgan('dev'));  // some development stuff 
 app.use(cookieParser()); // cookie parser
 app.set('view engine', 'ejs'); // setting up view engine for emails
+app.use('/', limiter); // rate limiter
 app.use('/api/v1', rootRoute)
 
 
